b6b98811d926ce00672e6b1302143dba1ea8c85e
balintvecsey <balint.vecsey@gmail.com>
2017-04-11 16:52:26 +0200

    add solution for Pirates

--- src/pirates/Pirates.java
+++ src/pirates/Pirates.java
@@ -1,8 +1,10 @@
+package pirates;
+
 import java.util.*;
 
-public class PirateCounter{
+class PirateCounter{
 
-  class Pirate {
+  static class Pirate {
     String name;
     boolean hasWoodenLeg;
     int gold;
@@ -19,15 +21,25 @@ public class PirateCounter{
 
     // Given this list...
 
-    pirates.add(new Pirate("Olaf", false, 12);
-    pirates.add(new Pirate("Uwe", true, 9);
-    pirates.add(new Pirate("Jack", true, 16);
-    pirates.add(new Pirate("Morgan", false, 17);
-    pirates.add(new Pirate("Hook", true, 20);
+    pirates.add(new Pirate("Olaf", false, 12));
+    pirates.add(new Pirate("Uwe", true, 9));
+    pirates.add(new Pirate("Jack", true, 16));
+    pirates.add(new Pirate("Morgan", false, 17));
+    pirates.add(new Pirate("Hook", true, 20));
 
     // Write a function that takes any list that contains pirates as in the example,
     // And returns a list of names containing the pirates that
     // - have wooden leg and
     // - have more than 15 gold
+    System.out.println(luckyPirate(pirates));
+  }
+
+  public static ArrayList<String> luckyPirate(ArrayList<Pirate> pirates) {
+    ArrayList<String> pirateNames = new ArrayList<>();
+    for (int i = 0; i < pirates.size(); i++) {
+      if (pirates.get(i).hasWoodenLeg && pirates.get(i).gold > 15) {
+        pirateNames.add(pirates.get(i).name);
+      }
+    } return pirateNames;
   }
 }
